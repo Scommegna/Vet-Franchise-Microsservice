f1a2d36efaa8fd08404b7253fdba80b59176bbb7
Lucas Scommegna <scommegnal@hotmail.com>
2023-07-25 14:38:23 -0300

    feat: created tutors and pet schema for mongodb

---
+++ src/models/pets.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+export const petSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Pet name must be provided."],
+  },
+  species: {
+    type: String,
+    required: [true, "Pet species must be provided."],
+  },
+  carry: {
+    type: String,
+    required: [true, "Pet carry must be provided."],
+  },
+  weight: {
+    type: Number,
+    required: [true, "Pet weight must be provided."],
+  },
+  date_of_birth: {
+    type: Date,
+    required: [true, "Pet date of birth must be provided."],
+  },
+});
---
+++ src/models/tutor.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+import { petSchema } from "./pets";
+
+// Tutors schema for database
+export const tutorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Tutor name must be provided."],
+  },
+  phone: {
+    type: String,
+    required: [true, "Tutor phone number must be provided."],
+  },
+  email: {
+    type: String,
+    required: [true, "Tutor email must be provided."],
+  },
+  date_of_birth: {
+    type: Date,
+    required: [true, "Tutor birth date must be provided"],
+  },
+  zip_code: {
+    type: String,
+    required: [true, "Tutor zipcode must be provided."],
+  },
+  pets: [petSchema],
+});
